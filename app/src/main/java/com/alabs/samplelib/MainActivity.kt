4dd7b561cdd5b7f393f25d770ce3492fd0629f26
wasdx19 <vanrwei@gmail.com>
2021-10-20 14:52:50 +0600

    LOOOLL

--- app/src/main/java/com/alabs/samplelib/MainActivity.kt
+++ app/src/main/java/com/alabs/samplelib/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         MessageUtil().showMessage(context = this@MainActivity,"Hellow")
+        MessageUtil().showMessage(context = this@MainActivity,"WHY?")
     }
 }
